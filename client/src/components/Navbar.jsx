0a6be8eafda1b3a9a93b8666490120b47e9292a7
Santosh <santuvishwakarma17@gmail.com>
2025-06-13 12:03:58 +0530

    Updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           <>
             <div className="navbar-header">
               <img src={logo} alt="Logo" className="navbar-logo" />
-              <h3>SKY Furaito</h3>
+              <h3>Airline Management System</h3>
             </div>
             <div className="nav-options">
               <p onClick={() => navigate('/')}>Home</p>
